69feca447e81089a7c64a42cdc66be4e1c353832
Pedromirandaa05 <pedrohenriquemiranda45@gmail.com>
2026-07-24 21:50:05 -0300

    Organização do treinar_modelos.py

--- agente_irrigacao/machine_learning/treinar_modelos.py
+++ agente_irrigacao/machine_learning/treinar_modelos.py
@@ -1,30 +1,96 @@
+
+import math
 import pandas as pd
 from pathlib import Path
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import (
+    mean_absolute_error,
+    mean_squared_error,
+    r2_score
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-arquivo = BASE_DIR / "dados" / "dados_tratados.csv"
+def carregar_dados():
+    arquivo = BASE_DIR / "dados" / "dados_tratados.csv"
 
-dados = pd.read_csv(arquivo)
+    dados = pd.read_csv(arquivo)
 
-X = dados[["Dia","Mes","Hora","Temperatura","Umidade"]]
+    return dados
 
-y_temperatura = dados["Temperatura_2h"]
+def dividir_dados(dados):
 
-y_umidade = dados["Umidade_2h"]
+    X = dados[["Dia", "Mes", "Hora", "Temperatura", "Umidade"]]
 
-X_train, X_test, y_temp_train, y_temp_test = train_test_split(X, y_temperatura, test_size=0.2,
-    shuffle=False)
+    y_temperatura = dados["Temperatura_2h"]
 
-X_train, X_test, y_umid_train, y_umid_test = train_test_split(X, y_umidade, test_size=0.2, shuffle=False)
+    y_umidade = dados["Umidade_2h"]
 
-print("=== Conjunto de treino ===")
-print(f"Entradas (X): {X_train.shape}")
-print(f"Temperatura (y): {y_temp_train.shape}")
-print(f"Umidade (y): {y_umid_train.shape}")
+    X_train, X_test, y_temp_train, y_temp_test = train_test_split(
+        X, y_temperatura, test_size=0.2, shuffle=False
+        )
 
-print("\n=== Conjunto de teste ===")
-print(f"Entradas (X): {X_test.shape}")
-print(f"Temperatura (y): {y_temp_test.shape}")
-print(f"Umidade (y): {y_umid_test.shape}")
+    X_train, X_test, y_umid_train, y_umid_test = train_test_split(
+        X, y_umidade, test_size=0.2, shuffle=False
+        )
+
+    return (X_train, X_test, y_temp_train, y_temp_test, y_umid_train, y_umid_test)
+
+def treinar_modelo(X_train, y_train):
+
+    modelo = DecisionTreeRegressor(random_state=42, max_depth=10)
+
+    modelo.fit(X_train, y_train)
+
+    return modelo
+
+def avaliar_modelo(y_real, y_previsto):
+
+    mae = mean_absolute_error(
+        y_real,
+        y_previsto
+    )
+
+    rmse = math.sqrt(
+        mean_squared_error(
+            y_real,
+            y_previsto
+        )
+    )
+
+    r2 = r2_score(
+        y_real,
+        y_previsto
+    )
+
+    return mae, rmse, r2
+
+def main():
+
+    dados = carregar_dados()
+
+    (X_train, X_test, y_temp_train, y_temp_test, y_umid_train, y_umid_test) = dividir_dados(dados)
+
+    modelo_temperatura = treinar_modelo(X_train, y_temp_train)
+    modelo_umidade = treinar_modelo(X_train, y_umid_train)
+
+    previsao_temperatura = modelo_temperatura.predict(X_test)
+    previsao_umidade = modelo_umidade.predict(X_test)
+
+    mae_temp, rmse_temp, r2_temp = avaliar_modelo(y_temp_test, previsao_temperatura)
+
+    mae_umid, rmse_umid, r2_umid = avaliar_modelo(y_umid_test, previsao_umidade)
+
+    print("\nTemperatura: ")
+    print(f"MAE  : {mae_temp:.2f}")
+    print(f"RMSE : {rmse_temp:.2f}")
+    print(f"R²   : {r2_temp:.3f}")
+
+    print("\nUmidade: ")
+    print(f"MAE  : {mae_umid:.2f}")
+    print(f"RMSE : {rmse_umid:.2f}")
+    print(f"R²   : {r2_umid:.3f}")
+
+if __name__ == "__main__":
+    main()
