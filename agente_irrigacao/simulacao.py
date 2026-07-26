5e70c0e016c23083115d4e7fbf9bd2a98dfef7f7
Pedromirandaa05 <pedrohenriquemiranda45@gmail.com>
2026-07-26 19:31:34 -0300

    Modificação do simulacao.py

--- agente_irrigacao/simulacao.py
+++ agente_irrigacao/simulacao.py
@@ -1,24 +1,42 @@
 from ambiente import Ambiente
 from agente import Agente
 
-agente = Agente()
 
-casos = [
-    Ambiente(10, 1, 20, 35, False, 19, "tomate"),
-    Ambiente(18, 3, 45, 25, False, 10, "alface"),
-    Ambiente(22, 7, 15, 32, False, 13, "milho"),
-    Ambiente(5, 9, 25, 30, False, 9, "mandioca"),
-    Ambiente(28, 11, 70, 22, True, 11, "arroz")
-]
+def main():
 
-# Testa cada cenário
-for i, ambiente in enumerate(casos, start=1):
+    agente = Agente()
 
-    acao, motivo, temp_prevista, umid_prevista = agente.decidir(ambiente)
+    casos = [
+        Ambiente(10, 1, 20, 35, False, 19, "tomate"),
+        Ambiente(18, 3, 45, 25, False, 10, "alface"),
+        Ambiente(22, 7, 15, 32, False, 13, "milho"),
+        Ambiente(5, 9, 25, 30, False, 9, "mandioca"),
+        Ambiente(28, 11, 70, 22, True, 11, "arroz")
+    ]
 
-    print(f"\nPrevisão para +2h:")
-    print(f"Temperatura prevista: {temp_prevista:.1f}°C")
-    print(f"Umidade prevista: {umid_prevista:.1f}%")
+    for i, ambiente in enumerate(casos, start=1):
 
-    print(f"Decisão: {acao}")
-    print(f"Motivo: {motivo}")
+        decisao, motivo, temp_prevista, umid_prevista = agente.decidir(ambiente)
+
+        print(f"CASO {i}")
+
+        print(f"Cultivo: {ambiente.cultivo.capitalize()}")
+        print(f"Data: {ambiente.dia:02d}/{ambiente.mes:02d}")
+        print(f"Horário: {ambiente.horario}:00")
+        print(f"Temperatura atual: {ambiente.temperatura:.1f} °C")
+        print(f"Umidade atual: {ambiente.umidade:.1f}%")
+        print(f"Chuva: {'Sim' if ambiente.chuva else 'Não'}")
+
+        print("\nPrevisão para +2 horas")
+        print(f"Temperatura prevista: {temp_prevista:.1f} °C")
+        print(f"Umidade prevista: {umid_prevista:.1f}%")
+
+        print("\nDecisão do agente")
+        print(f"Ação: {decisao}")
+        print(f"Motivo: {motivo}")
+
+        print()
+
+
+if __name__ == "__main__":
+    main()
